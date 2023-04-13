6a022f5e4ad7b9ba833dec483418f50416269508
bm4656 <newjin46@gmail.com>
2023-04-13 18:24:31 +0900

    Server Components

--- note/src/app/page.tsx
+++ note/src/app/page.tsx
@@ -1,10 +1,13 @@
-import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import styles from './page.module.css';
-import { notFound } from 'next/navigation';
+import os from 'os'; //노드 APIs
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  console.log('안녕!');
+  console.log(os.hostname());
+
+  // const [name, setName] = useState(''); 💩오류
   return <h1>홈페이지다아!</h1>;
 }
