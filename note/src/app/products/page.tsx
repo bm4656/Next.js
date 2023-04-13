2c1dda506328bc33e890af16eb178670f9240b7d
bm4656 <newjin46@gmail.com>
2023-04-13 16:42:07 +0900

    Link 2

--- note/src/app/products/page.tsx
+++ note/src/app/products/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import Link from 'next/link';
+
+const products = ['shirt', 'pants', 'skirt', 'shoes'];
 
 export default function ProductsPage() {
   return (
     <div>
       <h1>Products Page!</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            <Link href={`products/${product}`}>{product}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
