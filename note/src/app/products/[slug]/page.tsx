b97624f382db13c34b128018ad0d339cbfc8f065
bm4656 <newjin46@gmail.com>
2023-04-19 18:09:49 +0900

    ISR rendering

--- note/src/app/products/[slug]/page.tsx
+++ note/src/app/products/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import { getProduct, getProducts } from '@/app/service/products';
 import { notFound } from 'next/navigation';
 
+export const revalidate = 3;
+
 type Props = {
   params: {
     slug: string;
