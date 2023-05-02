fa88b06ec43dc972f5170d6a93d5e8d9c8f04be2
bm4656 <newjin46@gmail.com>
2023-05-03 03:10:59 +0900

    Image in next.js

--- note/src/app/page.tsx
+++ note/src/app/page.tsx
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import styles from './page.module.css';
 import os from 'os'; //노드 APIs
 import Counter from '@/components/Counter';
+import Image from 'next/image';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,6 +13,12 @@ export default function Home() {
   return (
     <>
       <h1>홈페이지다아!</h1>
+      <Image
+        src={'https://images.unsplash.com/photo-1441986300917-64674bd600d8'}
+        alt='homeImage'
+        width={400}
+        height={400}
+      />
       <Counter />
     </>
   );
--- note/src/app/products/page.tsx
+++ note/src/app/products/page.tsx
@@ -1,16 +1,18 @@
 import Link from 'next/link';
 import { getProducts } from '../service/products';
 import MeawArticle from '@/components/MeawArticle';
+import Image from 'next/image';
+import clothesImage from '../../../public/images/clothes.jpg';
 
 // export const revalidate = 3;
 
 export default async function ProductsPage() {
-  throw new Error();
   const products = await getProducts();
 
   return (
     <div>
-      <h1>Products Page!</h1>
+      <h1>제품 소개 페이지!</h1>
+      <Image src={clothesImage} alt='Clothes' />
       <ul>
         {products.map((product, index) => (
           <li key={index}>
